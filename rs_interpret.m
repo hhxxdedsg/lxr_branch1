19872936a228f8a016c9d6ca463b35db6f109b09
hhxxdedsg <2066814967@qq.com>
2022-11-24 20:04:08 +0800

    lixinrui V2

--- rs_interpret.m
+++ rs_interpret.m
@@ -1,4 +1,4 @@
- %得到码距最短的码字：
+%得到码距最短的码字：
 function interpret_msg=rs_interpret(in_msg,all_code_msg,all_msg)
 for i=1:512
  rs_dis(i)=rscode_dis(in_msg,all_code_msg(i,:)); 
@@ -7,3 +7,4 @@ rs_dis_min =min(rs_dis);
 index = find(rs_dis == rs_dis_min);
 interpret_msg = all_msg(index(1),:);
 return;
+
